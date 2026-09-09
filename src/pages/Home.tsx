42971bf7a7615e2b17f593d46f76d2cb87c50017
Kalab Tadesse <kalupt9999@gmail.com>
2026-09-09 10:10:20 -0700

    fsfas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -341,7 +341,6 @@ export default function Home({ onBook }: { onBook: () => void }) {
               <X size={20} />
             </button>
             <p className="eyebrow">Featured video</p>
-            <h2>Experience Lem Hotel</h2>
             <div className="video-modal-frame">
               <iframe
                 src="https://www.youtube.com/embed/sEAx9YeNQrw?autoplay=1&rel=0&modestbranding=1"
