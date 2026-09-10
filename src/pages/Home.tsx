ff04089f70aa4d1f0ee3f69b6975556d513ca90c
Kalab Tadesse <kalupt9999@gmail.com>
2026-09-09 19:59:00 -0700

    hero fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -107,16 +107,6 @@ export default function Home({ onBook }: { onBook: () => void }) {
   }, []);
 
   useEffect(() => {
-    const cycle = window.setInterval(() => {
-      setHeroIndex((current) => (current + 1) % heroImages.length);
-    }, 4200);
-  
-    return () => window.clearInterval(cycle);
-  }, []);
-
-
-
-  useEffect(() => {
     if (!isVideoOpen) {
       return;
     }
